ed8414adbc81644ea0a4a86e78848382bbf39aff
mokhles95 <mokhleshaj@gmail.com>
2021-06-18 12:36:54 +0100

    docker change

--- src/config/redis-init.ts
+++ src/config/redis-init.ts
@@ -1,28 +1,29 @@
-import redis from 'redis';
-import JWTR from 'jwt-redis';
-import { promisify } from 'util';
+import redis from "redis";
+import JWTR from "jwt-redis";
+import { promisify } from "util";
 
 export const client = redis.createClient({
-  port: 6379,
-  host: 'localhost',
+  port: 53260,
+  host: "localhost",
+  password: "yS6B4C6eKCEgV7KxB7gxNZnW2BKY6N5b",
 });
 
 export const jwtr = new JWTR(client);
 
 export const getClientAsync = promisify(client.get).bind(client);
 
-client.on('connect', () => {
-  console.log('client connected');
+client.on("connect", () => {
+  console.log("client connected");
 });
 
-client.on('error', err => {
+client.on("error", (err) => {
   console.log(err.message);
 });
 
-client.on('end', () => {
-  console.log('client disconnected from redis');
+client.on("end", () => {
+  console.log("client disconnected from redis");
 });
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   client.quit();
 });
